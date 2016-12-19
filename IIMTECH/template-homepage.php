f69fbae929bbfa4d6ee233fa3e42be9ef7aa6fb2
Mundi <MundiLimos@Gabriels-MacBook-Pro.local>
2016-12-18 22:17:01 -0500

    new changes

--- IIMTECH/template-homepage.php
+++ IIMTECH/template-homepage.php
@@ -8,43 +8,56 @@
  ?>
 
 
-
 <section class="section2">
     <div class="container">
         <div class="col-md-4">
-            <img src="/" />
-            <h3> Hello world</h3>
-            <img src="/" class="stars" />
-            <p></p>
+            <img src="/wp-content/themes/IIMTECH/images/hands.jpg" />
+            <h3> ALL - INCLUSIVE RATES</h3>
+            <img src="/wp-content/themes/IIMTECH/images/stars.jpg" class="stars" />
+            <p> Contrary to popular belief, Lorem Ipsum is not </p>
         </div>
         <div class="col-md-4">
-            <img src="/" />
-            <h3> Hello world</h3>
-            <img src="/" class="stars" />
-            <p></p>
+            <img src="/wp-content/themes/IIMTECH/images/wheel.jpg" />
+            <h3> PROFESSIONAL CHAUFFEURS</h3>
+            <img src="/wp-content/themes/IIMTECH/images/stars.jpg" class="stars" />
+            <p> Contrary to popular belief, Lorem Ipsum is not  </p>
         </div>
         <div class="col-md-4">
-            <img src="/" />
-            <h3> Hello world</h3>
-            <img src="/" class="stars" />
-            <p></p>
+            <img src="/wp-content/themes/IIMTECH/images/clock.jpg" />
+            <h3> ALWAYS ON TIME</h3>
+            <img src="/wp-content/themes/IIMTECH/images/stars.jpg" class="stars" />
+            <p> Contrary to popular belief, Lorem Ipsum is not  </p>
         </div>
         <div class="col-md-4">
-            <img src="/" />
-            <h3> Hello world</h3>
-            <img src="/" class="stars" />
-            <p></p>
+            <img src="/wp-content/themes/IIMTECH/images/phone.jpg"/>
+            <h3> 24 HOUR CUSTOMER SUPPORT </h3>
+            <img src="/wp-content/themes/IIMTECH/images/stars.jpg" class="stars" />
+            <p> Contrary to popular belief, Lorem Ipsum is not  </p>
         </div>
     </div>
 </section>
+
 <section class="section3">
-    <img src="/" class="videoimage" />
+    <img src="/wp-content/themes/IIMTECH/images/laptops.jpg"/>
     <p>
-        Hello
-        <br />
-        world
+        <a href="/video">Play presentation video</a>
+        <a href="/onlineb">Play presentation video</a>
+        <a href="/downloadapp">Play presentation video</a>
     </p>
+    <div class="container">
+        <div class="row">
+            <div class="col80">
+            <div class="col-md-6">
+                <img class="apple" src="/wp-content/themes/IIMTECH/images/apple.jpg"/>
+            </div>
+            <div class="col-md-6">
+                <img class="android" src="/wp-content/themes/IIMTECH/images/android.jpg"/>
+            </div>
+            </div>
+        </div>
+    </div>
 </section>
+
 <section class="sub section4">
     <h2 class="text-white text-center">   SUBSCRIBE FOR SPECIAL OFFERS</h2>
     <!-- sub goes here -->
