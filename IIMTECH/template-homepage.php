96d6b2959da412cb38fad4ff5263db1365903e47
Mundi <MundiLimos@Gabriels-MacBook-Pro.local>
2016-12-19 01:06:24 -0500

    enw

--- IIMTECH/template-homepage.php
+++ IIMTECH/template-homepage.php
@@ -36,15 +36,30 @@
             <p> Contrary to popular belief, Lorem Ipsum is not  </p>
         </div>
     </div>
+
+    <br /><br />
+
+
 </section>
 <br /><br />
 <section class="section3">
+
+    <br /><br />
+
     <img src="/wp-content/themes/IIMTECH/images/laptops.jpg"/>
     <p>
+        <br /><br />
+
         <a href="/video">Play presentation video</a>
         <a href="/onlineb">Play presentation video</a>
         <a href="/downloadapp">Play presentation video</a>
+
+        <br /><br />
+
     </p>
+
+
+
     <div class="container">
         <div class="row">
             <div class="col80">
@@ -72,7 +87,7 @@
     <hr class="smallhr">
     <h3 class="text-white text-center"> THE PERFECT SEAT FOR ANY JOURNEY </h3>
     <br />
-    <p>
+    <p class="centerp">
 
         Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
 
@@ -81,7 +96,7 @@
 
 <br /><br />
 
-
+<!--
 <section class="section6" id="discovermore">
     <h2> Limo Limo Limo Limo Limo Limo Limo  </h2>
     <br />
@@ -91,7 +106,7 @@
 
 
 <br /><br />
-
+-->
 
         <?php get_footer(); ?>
 
