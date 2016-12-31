13c02269c55efac3732ee86c160330e1c38f80cf
Mundi <MundiLimos@Gabriels-MacBook-Pro.local>
2016-12-31 14:57:41 -0500

    changes

--- IIMTECH/template-contact.php
+++ IIMTECH/template-contact.php
@@ -39,8 +39,8 @@
 
     <div class="text-center">
 
-<div style="max-width: 800px; margin: auto; width: 100%">
-    
+<div style="max-width: 800px; margin: auto; width: 100%" class="contactpagelinks">
+
         <div class="col-md-6"> <a href="tel:8003177087" class="cta">  <i class="fa fa-phone" aria-hidden="true"></i>   &nbsp; 800.317.7087 </a> </div>
         <div class="col-md-6"> <a href="mailto:info@mundilimos.com" class="cta"> <i class="fa fa-envelope" aria-hidden="true"> </i>  &nbsp; info@mundilimos.com</a> </div>
 
@@ -49,7 +49,11 @@
     </div>
 
 
-    <h4 class="text-white text-center">
+    <h4 class="text-white text-center" style="    font-size: 16px;
+    max-width: 700px;
+    margin: auto;
+    font-weight: 100;
+    padding: 10px;">
 
         Allow our team of seasoned travel and transportation professionals to assist you. Around the corner or around the globe, our operators are standing by 24 hours a day.
 
