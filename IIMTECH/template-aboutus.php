3fd9b5a80b0fd5fc24f6572a33be03b83f50606e
Mundi <MundiLimos@Gabriels-MacBook-Pro.local>
2017-01-02 20:18:39 -0500

    fix

--- IIMTECH/template-aboutus.php
+++ IIMTECH/template-aboutus.php
@@ -9,23 +9,54 @@ get_header();
 
 
 
+    <section class="section5" id="fleet">
+         <h3 class="text-white text-center"> THE PERFECT SEAT FOR ANY JOURNEY </h3>
+        <br>
+        <p class="centerp">
+
+            Held to the highest standards of safety, maintenance, and presentation, our extensive fleet features only the market’s latest model luxury vehicles, allowing us to offer the right selection to suit the specifics needs of our clients. You can choose from a variety of top class sedans, SUVs, stretch limousines, vans and coaches. All vehicles are black on black colored, with full leather interiors and tinted windows to ensure comfort and privacy. While being transferred by one of our professional chauffeurs, you are empowered to relax, enjoy the finest on-the-go amenities and take full advantage of the industry’s most advanced technologies that provides non-stop productivity for private and corporate travelers. Whether your transportation requirements are for airport transfer, corporate events or entertainment, Mundi Limousine delivers the perfect seat for any journey.
+        </p>
+    </section>
 
 
-    <section class="section5">
+    <section class="section5 bgglobe">
 
         <div class="container">
 
+
+
+
             <div class="col-md-4">
-
-
+                 <div class="aboutusbox">
+                    <img src="/wp-content/themes/IIMTECH/images/Mundicrown.jpg" class="iconicon"/>
+                    <span class="text-center text-white" style="text-transform: uppercase; display: block;"> Hello </span>
+                    <br/>
+                    <p class="text-center centerp">
+                        Held to the highest standards of safety, maintenance, and presentation, our extensive fleet features only the market’s latest model luxury vehicles, allowing us to offer the right selection to suit the specifics needs of our clients.
+                    </p>
+                </div>
 
             </div>
             <div class="col-md-4">
-
+                <div class="aboutusbox">
+                    <img src="/wp-content/themes/IIMTECH/images/Mundicrown.jpg" class="iconicon"/>
+                    <span class="text-center text-white" style="text-transform: uppercase; display: block;"> Hello </span>
+                    <br/>
+                    <p class="text-center centerp">
+                        Held to the highest standards of safety, maintenance, and presentation, our extensive fleet features only the market’s latest model luxury vehicles, allowing us to offer the right selection to suit the specifics needs of our clients.
+                    </p>
+                </div>
 
             </div>
             <div class="col-md-4">
-
+                <div class="aboutusbox">
+                    <img src="/wp-content/themes/IIMTECH/images/Mundicrown.jpg" class="iconicon"/>
+                    <span class="text-center text-white" style="text-transform: uppercase; display: block;"> Hello </span>
+                    <br/>
+                    <p class="text-center centerp">
+                        Held to the highest standards of safety, maintenance, and presentation, our extensive fleet features only the market’s latest model luxury vehicles, allowing us to offer the right selection to suit the specifics needs of our clients.
+                    </p>
+                </div>
 
 
             </div>
@@ -34,7 +65,23 @@ get_header();
 
         </div>
 
+
+
+
+
+
     </section>
+
+
+    <section class="section5">
+        <h3 class="text-white text-center"> THE PERFECT SEAT FOR ANY JOURNEY </h3>
+        <br>
+        <p class="centerp">
+
+            Held to the highest standards of safety, maintenance, and presentation, our extensive fleet features only the market’s latest model luxury vehicles, allowing us to offer the right selection to suit the specifics needs of our clients. You can choose from a variety of top class sedans, SUVs, stretch limousines, vans and coaches. All vehicles are black on black colored, with full leather interiors and tinted windows to ensure comfort and privacy. While being transferred by one of our professional chauffeurs, you are empowered to relax, enjoy the finest on-the-go amenities and take full advantage of the industry’s most advanced technologies that provides non-stop productivity for private and corporate travelers. Whether your transportation requirements are for airport transfer, corporate events or entertainment, Mundi Limousine delivers the perfect seat for any journey.
+        </p>
+    </section>
+
 
 
 
