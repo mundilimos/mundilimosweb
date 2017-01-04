cfc52109878e99667f5da5fdc474e11e37187433
Mundi <MundiLimos@Gabriels-MacBook-Pro.local>
2017-01-03 23:43:06 -0500

    template bigbig

---
+++ IIMTECH/template-bigbig.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: BIGBIG Template
+ */
+
+get_header();
+
+?>
+
+
+
+    <section class="section5 centerp" id="fleet">
+
+
+
+        <?php the_content(); ?>
+
+
+    </section>
+
+
+
+
+
+
+<?php get_footer(); ?>
