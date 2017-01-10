8ad7137fdabc00d1df043bb909ed0917bcf7460e
Mundi <MundiLimos@Gabriels-MacBook-Pro.local>
2017-01-09 23:41:06 -0500

    trying both

--- IIMTECH/template-internal.php
+++ IIMTECH/template-internal.php
@@ -81,7 +81,7 @@ get_header();
 
         <!-- Tab panes -->
 
-        <ul class="tab-content" style="padding-bottom: 30px;">
+        <ul class="slickNow tab-content" style="padding-bottom: 30px;">
 
 
 
